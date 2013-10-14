d209fcd493436a43ee45ee14c8bbd7b6ef0501ff
Tomas Barton <barton.tomas@gmail.com>
2013-10-14 15:11:31 +0200

    storage specs

--- manifests/storage.pp
+++ manifests/storage.pp
@@ -3,18 +3,33 @@
 # This module manages FhGFS client
 #
 class fhgfs::storage (
-  $enable            = false,
-  $storage_directory = $fhgfs::params::storage_directory,
-  $mgmtd_host        = $fhgfs::params::mgmtd_host,
-  $version           = $fhgfs::params::version,
+  $enable            = true,
+  $storage_directory = $fhgfs::storage_directory,
+  $mgmtd_host        = $fhgfs::mgmtd_host,
+  $version           = $fhgfs::version,
+  $user              = $fhgfs::user,
+  $group             = $fhgfs::group,
 ) inherits fhgfs {
+
+  require fhgfs::install
 
   package { 'fhgfs-storage':
     ensure => $version,
   }
 
+  file { $storage_directory:
+    ensure  => directory,
+    owner   => $user,
+    group   => $group,
+    recurse => true,
+  }
+
   file { '/etc/fhgfs/fhgfs-storage.conf':
     require => Package['fhgfs-storage'],
+    ensure  => present,
+    owner   => $user,
+    group   => $group,
+    mode    => '0755',
     content => template('fhgfs/fhgfs-storage.conf.erb'),
   }
 
---
+++ spec/classes/fhgfs_storage_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe 'fhgfs::storage' do
+  shared_examples 'debian-storage' do |os, codename|
+    let(:facts) {{
+      :operatingsystem => os,
+      :osfamily => 'Debian',
+      :lsbdistcodename => codename,
+    }}
+    it { should contain_package('fhgfs-storage') }
+    it { should contain_package('fhgfs-utils') }
+
+    it { should contain_service('fhgfs-storage').with(
+        :ensure => 'running',
+        :enable => true
+    ) }
+
+    it { should contain_file('/etc/fhgfs/fhgfs-storage.conf').with({
+      'ensure'  => 'present',
+      'owner'   => user,
+      'group'   => group,
+      'mode'    => '0755',
+      'require' => 'Package[fhgfs-storage]',
+    }) }
+
+
+    it { should contain_file('/storage').with({
+      'ensure'  => 'directory',
+      'owner'   => user,
+      'group'   => group,
+    }) }
+
+  end
+
+  context 'on debian-like system' do
+    let(:user) { 'fhgfs' }
+    let(:group) { 'fhgfs' }
+
+    let('fhgfs::storage_directory') { '/storage' }
+
+    it_behaves_like 'debian-storage', 'Debian', 'squeeze'
+    it_behaves_like 'debian-storage', 'Debian', 'wheezy'
+    it_behaves_like 'debian-storage', 'Ubuntu', 'precise'
+  end
+
+end
