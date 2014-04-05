479c9bb8f47f8dda9506dbcf60a2a22a362ef7a3
Tomas Barton <barton.tomas@gmail.com>
2014-04-05 12:58:19 +0200

    removed explicit hiera calls

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,22 +11,22 @@
 # Copyright 2013 Tomas Barton, unless otherwise noted.
 #
 class fhgfs {
-  $manage_repo                   = hiera('fhgfs::manage_repo', true)
-  $mgmtd_host                    = hiera('fhgfs::mgmtd_host', 'localhost')
-  $meta_directory                = hiera('fhgfs::meta_directory', '/meta')
-  $storage_directory             = hiera('fhgfs::storage_directory', '/storage')
-  $client_auto_remove_mins       = hiera('fhgfs::client_auto_remove_mins', 0)
-  $meta_space_low_limit          = hiera('fhgfs::meta_space_low_limit', '5G')
-  $meta_space_emergency_limit    = hiera('fhgfs::meta_space_emergency_limit', '3G')
-  $storage_space_low_limit       = hiera('fhgfs::storage_space_low_limit', '100G')
-  $storage_space_emergency_limit = hiera('fhgfs::storage_space_emergency_limit', '10G')
-  $package_source                = hiera('fhgfs::package_source', 'fhgfs')
-  $version                       = hiera('fhgfs::version', undef)
-  $log_dir                       = hiera('fhgfs::log_dir', '/var/log/fhgfs')
-  $user                          = hiera('fhgfs::user', 'root')
-  $group                         = hiera('fhgfs::group','root')
-  $major_version                 = hiera('fhgfs::major_version','fhgfs_2012.10')
-  $admon_db_file                 = hiera('fhgfs::admon_db_file','/var/lib/fhgfs/fhgfs-admon.db')
+  $manage_repo                   = true
+  $mgmtd_host                    = 'localhost'
+  $meta_directory                = '/meta'
+  $storage_directory             = '/storage'
+  $client_auto_remove_mins       = 0
+  $meta_space_low_limit          = '5G'
+  $meta_space_emergency_limit    = '3G'
+  $storage_space_low_limit       = '100G'
+  $storage_space_emergency_limit = '10G'
+  $package_source                = 'fhgfs'
+  $version                       = undef
+  $log_dir                       = '/var/log/fhgfs'
+  $user                          = 'root'
+  $group                         = 'root'
+  $major_version                 = 'fhgfs_2012.10'
+  $admon_db_file                 = '/var/lib/fhgfs/fhgfs-admon.db'
 
   if ($version == undef){
     $package_ensure = 'present'
